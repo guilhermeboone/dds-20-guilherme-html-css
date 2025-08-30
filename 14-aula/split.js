e06c2de5d94ad4324eb78f13672b38d531127787
GUILHERME GALVÃO BOONE <g.boone@labcetec.net>
2025-08-29 21:31:52 -0300

    a

---
+++ 14-aula/split.js
@@ -0,0 +1,8 @@
+// Separa uma string de acordo com o caracter separador especificado.
+
+// Exemplo:
+
+    let email = "usuario@exemplo.com";
+    let partesEmail = email.split("@");
+    console.log("Antes: ",email)
+    console.log("Depois: ",partesEmail)
